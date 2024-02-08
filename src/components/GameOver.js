5ae9ee133175b0a4a51b0184793980fc8a7acdc4
Mayon-Francis <mayonfrancis@gmail.com>
2024-02-08 11:33:52 +0530

    Responsiveness

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -16,7 +16,7 @@ function GameOver({
         <h1>Correct Word: {correctWord}</h1>
       )}
       {gameOver.guessedWord && (
-        <h3>You guessed in {currAttempt.attempt} attempts</h3>
+        <h3>You guessed in {currAttempt.attempt} {currAttempt.attempt === 1 ? 'attempt' : 'attempts'}</h3>
       )}
     </div>
   );
